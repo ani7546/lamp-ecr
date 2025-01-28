46157eede6ffb9aa087a17b149b831ce99c81208
ani7546 <142499787+ani7546@users.noreply.github.com>
2025-01-28 13:28:13 +0530

    Update login.php

--- login.php
+++ login.php
@@ -113,7 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         ?>
         <div class="form-wrap border rounded p-4">
           <h1>Log In</h1>
-          <p> login to continue1</p>
+          <p> login to continue</p>
           <!-- form starts here -->
           <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" novalidate>
             <div class="mb-3">
